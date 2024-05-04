9d95660074dfc2481182c053a3a13534f50c29d0
Fadilix <iamfadile2@gmail.com>
2024-05-04 16:23:28 +0000

    added rest of data

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -11,4 +11,27 @@ List<Map> data = [
     "src": "${f}ps_pulse_earphone.png",
     "favorite": false
   },
+  {"name": "PS Vita 2", "src": "${f}ps_vita_2.png", "favorite": false},
+  {"name": "PS4 Controller", "src": "${f}ps_controller.png", "favorite": false},
+  {"name": "PS4 Slim Pro", "src": "${f}ps_slim_pro.png", "favorite": false},
+  {
+    "name": "PS5 Charging Station",
+    "src": "${f}ps_charging_station.png",
+    "favorite": false
+  },
+  {"name": "PS5 Controller", "src": "${f}ps_controller.png", "favorite": false},
+  {
+    "name": "PS5 Digital Edition",
+    "src": "${f}ps_digital_edition.png",
+    "favorite": false
+  },
+  {"name": "PS5 Remote", "src": "${f}ps_remote.webp", "favorite": false},
+  {"name": "Pulse Headset", "src": "${f}pulse_headset.png", "favorite": false},
+  {
+    "name": "Sony Headphone",
+    "src": "${f}sony_headphone.png",
+    "favorite": false
+  },
+  {"name": "Spider-Man 2", "src": "${f}spider_man_2.png", "favorite": false},
+  {"name": "Tekken 7", "src": "${f}tekken7.png", "favorite": false}
 ];
